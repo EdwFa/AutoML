eb47e901ccce06a7baeebae399ef67d55e6c34b7
Ufifus <artemnesteihyb@mail.ru>
2023-09-21 16:06:04 +0300

    add docs

--- accounts/models.py
+++ accounts/models.py
@@ -35,7 +35,7 @@ class User(AbstractUser):
         return reverse('accounts:user-account', kwargs={'user_id': self.id})
 
     def save(self, *args, **kwargs):
-        user_folder = os.path.join('base_datasets', self.username)
+        user_folder = os.path.join('datasets', self.username)
         super().save(*args, **kwargs)
 
         if not os.path.exists(user_folder):
@@ -50,7 +50,7 @@ class User(AbstractUser):
                     info=basic_dataset,
                     path=""
                 )
-                path_to_save_folder = os.path.join('base_datasets', self.username,
+                path_to_save_folder = os.path.join('datasets', self.username,
                                                    f'{dataset_table.id}_{dataset_table.name}')
                 os.mkdir(path_to_save_folder)
                 dataset_table.path = path_to_save_folder
@@ -60,6 +60,6 @@ class User(AbstractUser):
     def delete(self, *args, **kwargs):
         for dataset in self.datasets.all():
             dataset.delete()
-        if os.path.exists(os.path.join('base_datasets', self.username)):
-            clear_dir(os.path.join('base_datasets', self.username))
+        if os.path.exists(os.path.join('datasets', self.username)):
+            clear_dir(os.path.join('datasets', self.username))
         super().delete(*args, **kwargs)
